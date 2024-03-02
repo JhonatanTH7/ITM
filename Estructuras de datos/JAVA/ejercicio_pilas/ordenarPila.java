6e6b54cc0df2509c5ae0ffd8795668b2cd663f34
JhonatanTH7 <jhonatantorohurtado93@gmail.com>
2024-03-02 13:29:01 -0500

    Update

--- Estructuras de datos/JAVA/ejercicio_pilas/ordenarPila.java
+++ Estructuras de datos/JAVA/ejercicio_pilas/ordenarPila.java
@@ -4,8 +4,8 @@ import java.util.Collections;
 import java.util.Stack;
 
 public class ordenarPila {
-    public void ascendente(Stack<Integer> pila) {
-        Collections.sort(pila);
-        System.out.println(pila);
+    public void ascendente(Stack<Integer> pilaAux) {
+        Collections.sort(pilaAux);
+        System.out.println(pilaAux);
     }
 }
