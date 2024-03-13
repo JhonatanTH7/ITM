6bf23ef4e74c6f9612021707e2af5c84c1af85c3
JhonatanTH7 <jhonatantorohurtado93@gmail.com>
2024-03-13 13:44:08 -0500

    Update FillMatrix

--- Java/EstructurasDeDatos/talleres/segundo_taller/src/Utilities/FillMatrix.java
+++ Java/EstructurasDeDatos/talleres/segundo_taller/src/Utilities/FillMatrix.java
@@ -5,8 +5,8 @@ public class FillMatrix {
     public int[][] fillIntMAtrix(int[][] matrix, int top, int botttom) {
         RandomNumber randomNumber = new RandomNumber();
         for (int[] row : matrix) {
-            for (int column : row) {
-                row[column] = randomNumber.generate(top, botttom);
+            for (int i = 0; i < row.length; i++) {
+                row[i] = randomNumber.generate(top, botttom);
             }
         }
         return matrix;
